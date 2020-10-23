7af9aa6b9c56898c1705acf856a38db55cdf8062
mattboan <mattboan97@gmail.com>
2020-10-23 12:07:52 +0800

    Final Commit

--- scrape.js
+++ scrape.js
@@ -3,6 +3,17 @@ const fs = require('fs');
 const colors = require('colors');
 const Scraper = require('images-scraper');
 
+//Get the arguments
+const args = process.argv.slice(2);
+const keyword = args[0];
+const amount = args[1];
+const downloadPath = __dirname + "/download/" + keyword + "/";
+
+//Create new file directory
+if (!fs.existsSync(downloadPath))
+	//fs.mkdirSync(downloadPath);
+	fs.mkdir(downloadPath, {recursive: true}, err => {})
+
 //Create a new scraper
 const google = new Scraper({
 	puppeteer: {
@@ -27,16 +38,18 @@ const download_image = (url, image_path) =>
 
 //Scrape using the scrapper then download every url
 (async () => {
-	const results = await google.scrape('apple', 10);
+	const results = await google.scrape(keyword, amount);
+	console.log("Got " + results.length + " results.");
 	for(var i = 0; i < results.length; i++) {
 		var obj = results[i];
 
 		try {
-			let example_image_1 = await download_image(obj.url, 'example-1');
+			let example_image_1 = await download_image(obj.url, downloadPath + keyword + i);
 			console.log("Done Downloading: ".green + obj.url);
 		} catch (err) {
 			console.log("Error: " + err.message);
 		}
+
 		console.log(obj.url);
 
 	}
